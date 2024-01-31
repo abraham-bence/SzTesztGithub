b91ba71bcd8798a18291058fee2ab6a645f520f2
Bencuska^^ <75310580+abraham-bence@users.noreply.github.com>
2024-01-31 13:28:43 +0100

    2. Feladat

--- SzTesztGitHub/Program.cs
+++ SzTesztGitHub/Program.cs
@@ -10,6 +10,25 @@ namespace SzTesztGitHub
     {
         static void Main(string[] args)
         {
+            List<int> numberList = new List<int>();
+            int numberInput = 0;
+            do
+            {
+                Console.WriteLine("Give a number to add to the list: ");
+                numberInput = int.Parse(Console.ReadLine());
+                if (numberInput != 0)
+                {
+                    numberList.Add(numberInput);
+                }
+            } while (numberInput != 0);
+
+
+            //foreach (int number in numberList)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            Console.ReadKey();
         }
     }
 }
